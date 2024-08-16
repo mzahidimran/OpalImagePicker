c8c7380a01ab042d18cc66eb43afa46ac77e0386
Muhammad Zahid Imran <33024798+zahidappsgenii@users.noreply.github.com>
2024-08-16 23:25:09 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,28 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "OpalImagePicker"
+    name: "OpalImagePicker",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "OpalImagePicker",
+            targets: ["OpalImagePicker"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "OpalImagePicker",
+            dependencies: [],
+            path: "OpalImagePicker/Source",
+            exclude: [],
+            resources: [
+                            .process("../SupportingFiles")
+                        ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
